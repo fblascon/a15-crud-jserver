1cc603dc6ce1be6de4515b544d71e02bf10b8e9c
Fernando Blasco <fblascon@gmail.com>
2025-04-03 10:20:02 +0200

    cambio el servicio por la data de glitch

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -6,23 +6,44 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class EmployeeService {
-  constructor(private _http: HttpClient) {}
+
+
+  private apiUrl = 'https://zippy-soft-heaven.glitch.me/employees';
+
+  constructor(private _http: HttpClient) { }
 
   addEmployee(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/employees', data);
+    return this._http.post(this.apiUrl, data);
   }
 
   updateEmployee(id: number, data: any): Observable<any> {
-    return this._http.put(`http://localhost:3000/employees/${id}`, data);
+    return this._http.put(`${this.apiUrl}/${id}`, data);
   }
 
   getEmployeeList(): Observable<any> {
-    return this._http.get('http://localhost:3000/employees');
+    return this._http.get(this.apiUrl);
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/employees/${id}`);
+    return this._http.delete(`${this.apiUrl}/${id}`);
   }
+  // constructor(private _http: HttpClient) {}
+
+  // addEmployee(data: any): Observable<any> {
+  //   return this._http.post('http://localhost:3000/employees', data);
+  // }
+
+  // updateEmployee(id: number, data: any): Observable<any> {
+  //   return this._http.put(`http://localhost:3000/employees/${id}`, data);
+  // }
+
+  // getEmployeeList(): Observable<any> {
+  //   return this._http.get('http://localhost:3000/employees');
+  // }
+
+  // deleteEmployee(id: number): Observable<any> {
+  //   return this._http.delete(`http://localhost:3000/employees/${id}`);
+  // }
 }
 
 
